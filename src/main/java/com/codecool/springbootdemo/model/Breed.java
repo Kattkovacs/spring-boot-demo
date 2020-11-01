46db41e02335bb166eb1520fe6b0e78a7ea340a0
Kattkovacs <kattkovacs@gmail.com>
2020-11-01 17:15:15 +0100

    Initial commit

---
+++ src/main/java/com/codecool/springbootdemo/model/Breed.java
@@ -0,0 +1,5 @@
+package com.codecool.springbootdemo.model;
+
+public enum Breed {
+    SPANIEL, CORGI, AKITA, TERRIER
+}
---
+++ src/main/java/com/codecool/springbootdemo/model/Dog.java
@@ -0,0 +1,51 @@
+package com.codecool.springbootdemo.model;
+
+import org.springframework.validation.annotation.Validated;
+
+public class Dog {
+    private Breed breed;
+    private String name;
+    private Integer age;
+
+    public Dog(Breed breed, String name, Integer age) {
+        this.breed = breed;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog() {
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "breed=" + breed +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
